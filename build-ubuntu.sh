2762063c90741870db9010b812539b7a783ac9c8
sinetek <pitwuu@gmail.com>
2016-01-19 13:18:25 +0700

    We use autobuild on those platforms.

--- build-ubuntu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# install and configure bdb 4.8 manually, from http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
-
-qmake "BDB_LIB_PATH=/usr/local/BerkeleyDB.4.8/lib" "BDB_INCLUDE_PATH=/usr/local/BerkeleyDB.4.8/include" "QMAKE_LFLAGS+=-static-libstdc++"
-nice make -j`nproc` 
-
