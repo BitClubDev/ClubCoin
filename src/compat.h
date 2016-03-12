5b192cf37867cddc153627d93f924fccf3ae68e7
sinetek <pitwuu@gmail.com>
2016-03-12 17:36:00 +0900

    SetThreadPriority(): sync w/ bitcoin

--- src/compat.h
+++ src/compat.h
@@ -51,6 +51,17 @@ typedef int socklen_t;
 #define SOCKET_ERROR        -1
 #endif
 
+#ifndef WIN32
+// PRIO_MAX is not defined on Solaris
+#ifndef PRIO_MAX
+#define PRIO_MAX 20
+#endif
+#define THREAD_PRIORITY_LOWEST          PRIO_MAX
+#define THREAD_PRIORITY_BELOW_NORMAL    2
+#define THREAD_PRIORITY_NORMAL          0
+#define THREAD_PRIORITY_ABOVE_NORMAL    (-2)
+#endif
+
 inline int myclosesocket(SOCKET& hSocket)
 {
     if (hSocket == INVALID_SOCKET)
--- src/util.cpp
+++ src/util.cpp
@@ -1217,6 +1217,19 @@ void RenameThread(const char* name)
 #endif
 }
 
+void SetThreadPriority(int nPriority)
+{
+#ifdef WIN32
+    SetThreadPriority(GetCurrentThread(), nPriority);
+#else // WIN32
+#ifdef PRIO_THREAD
+    setpriority(PRIO_THREAD, 0, nPriority);
+#else // PRIO_THREAD
+    setpriority(PRIO_PROCESS, 0, nPriority);
+#endif // PRIO_THREAD
+#endif // WIN32
+}
+
 std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime)
 {
     // std::locale takes ownership of the pointer
