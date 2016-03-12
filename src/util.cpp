818b8b1675da0cf3173c9433f6b5733847a39bd5
sinetek <pitwuu@gmail.com>
2016-03-12 17:08:20 +0900

    bitcoin/commit/9b637589740089a56a81a79d38f4be522cfa43c3

--- src/util.cpp
+++ src/util.cpp
@@ -942,7 +942,6 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
     std::string message = FormatException(pex, pszThread);
     LogPrintf("\n\n************************\n%s\n", message);
     fprintf(stderr, "\n\n************************\n%s\n", message.c_str());
-    strMiscWarning = message;
 }
 
 boost::filesystem::path GetDefaultDataDir()
