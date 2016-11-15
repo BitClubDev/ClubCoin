8f7dcb38beca01feac4f7fc5bf36f3e134026425
sinetek <sinetek@sineteks-MacBook-Air.local>
2016-11-14 22:20:20 -0500

    UtilityDialog: Add shutdown window.

---
+++ src/qt/utilitydialog.cpp
@@ -0,0 +1,66 @@
+// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#if defined(HAVE_CONFIG_H)
+#include "config/bitcoin-config.h"
+#endif
+
+#include "utilitydialog.h"
+
+//#include "ui_helpmessagedialog.h"
+
+#include "bitcoingui.h"
+#include "clientmodel.h"
+#include "guiconstants.h"
+//#include "intro.h"
+//#include "paymentrequestplus.h"
+#include "guiutil.h"
+#include "bitcoingui.h"
+
+#include "clientversion.h"
+#include "init.h"
+#include "util.h"
+
+#include <stdio.h>
+
+#include <QCloseEvent>
+#include <QLabel>
+#include <QRegExp>
+#include <QTextTable>
+#include <QTextCursor>
+#include <QVBoxLayout>
+
+/** "Shutdown" window */
+ShutdownWindow::ShutdownWindow(QWidget *parent, Qt::WindowFlags f):
+    QWidget(parent, f)
+{
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(new QLabel(
+        tr("%1 is shutting down...").arg(tr(PACKAGE_NAME)) + "<br /><br />" +
+        tr("Do not shut down the computer until this window disappears.")));
+    setLayout(layout);
+}
+
+void ShutdownWindow::showShutdownWindow(BitcoinGUI *window)
+{
+    if (!window)
+        return;
+
+    // Show a simple window indicating shutdown status
+    QWidget *shutdownWindow = new ShutdownWindow();
+    // We don't hold a direct pointer to the shutdown window after creation, so use
+    // Qt::WA_DeleteOnClose to make sure that the window will be deleted eventually.
+    shutdownWindow->setAttribute(Qt::WA_DeleteOnClose);
+    shutdownWindow->setWindowTitle(window->windowTitle());
+
+    // Center shutdown window at where main window was
+    const QPoint global = window->mapToGlobal(window->rect().center());
+    shutdownWindow->move(global.x() - shutdownWindow->width() / 2, global.y() - shutdownWindow->height() / 2);
+    shutdownWindow->show();
+}
+
+void ShutdownWindow::closeEvent(QCloseEvent *event)
+{
+    event->ignore();
+}
---
+++ src/qt/utilitydialog.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QT_UTILITYDIALOG_H
+#define BITCOIN_QT_UTILITYDIALOG_H
+
+#include <QDialog>
+#include <QObject>
+
+class BitcoinGUI;
+
+/** "Shutdown" window */
+class ShutdownWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ShutdownWindow(QWidget *parent=0, Qt::WindowFlags f=0);
+    static void showShutdownWindow(BitcoinGUI *window);
+
+protected:
+    void closeEvent(QCloseEvent *event);
+};
+
+#endif // BITCOIN_QT_UTILITYDIALOG_H
